efdf49e4515a5f0e81d8d7f17c67aee07457d88d
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-28 21:13:45 +0530

    User res

--- Swagger/src/main/java/com/spring/api/documentation/Swagger/SwaggerApplication.java
+++ Swagger/src/main/java/com/spring/api/documentation/Swagger/SwaggerApplication.java
@@ -2,8 +2,14 @@ package com.spring.api.documentation.Swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@ComponentScan({"com.spring.api.documentation.*"})
+@EntityScan("com.spring.api.documentation.*")
 public class SwaggerApplication {
 
 	public static void main(String[] args) {
