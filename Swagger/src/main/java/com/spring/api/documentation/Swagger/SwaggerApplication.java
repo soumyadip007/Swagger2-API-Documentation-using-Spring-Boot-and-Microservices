fdd8489041e699f416493d48fc5986f681bc80d7
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-28 20:58:48 +0530

    Swagger Application

---
+++ Swagger/src/main/java/com/spring/api/documentation/Swagger/SwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.api.documentation.Swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+
+}
